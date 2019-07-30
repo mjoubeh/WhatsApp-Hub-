a668ba033155b702162cccc836d705a187f8a17d
Mohanad Joubeh <m.joubeh@yahoo.com>
2019-07-30 08:06:10 -0700

    tes

--- WhatsAppHub/src/main/java/com/ig/whatsapp/hub/WhatsAppHubApplication.java
+++ WhatsAppHub/src/main/java/com/ig/whatsapp/hub/WhatsAppHubApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WhatsAppHubApplication {
-
+//test
 	public static void main(String[] args) {
 		SpringApplication.run(WhatsAppHubApplication.class, args);
 	}
