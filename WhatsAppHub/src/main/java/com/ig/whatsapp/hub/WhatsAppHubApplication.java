f2b29ee43a688551e02596f49ddbbd1c006c902d
Mohanad Joubeh <m.joubeh@yahoo.com>
2019-07-31 09:42:51 -0700

    test
    test

--- WhatsAppHub/src/main/java/com/ig/whatsapp/hub/WhatsAppHubApplication.java
+++ WhatsAppHub/src/main/java/com/ig/whatsapp/hub/WhatsAppHubApplication.java
@@ -1,11 +1,26 @@
 package com.ig.whatsapp.hub;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class WhatsAppHubApplication {
 //test
+
+	@Value("${PORT:8080}")
+	String port;
+
+	@RestController
+	class HelloworldController {
+		@GetMapping("/")
+		String hello() {
+			return "Hello " + "!";
+		}
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(WhatsAppHubApplication.class, args);
 	}
